58c8d68f251f0e9b6fdbfded82b2482df4d9de63
erictan0429@gmail.com <erictan0429@gmail.com>
2021-08-28 20:15:57 +1200

    Fixed mobile section 7

--- client/src/components/MobileView/MobileHomeSection7.jsx
+++ client/src/components/MobileView/MobileHomeSection7.jsx
@@ -1,25 +1,17 @@
 import React, { Component } from "react";
-import office from "../../assets/office3.webp";
 import backgroundImg from "../../assets/abstract.webp";
 import office2 from "../../assets/mobileOffice1.webp";
+import robot from "../../assets/mobile/section7/robot.webp";
+import robot1 from "../../assets/mobile/section7/robot1.webp";
+import dataGif from "../../assets/mobile/section7/data.webm";
 
 class MobileHomeSection7 extends Component {
   render() {
     return (
       <section className="section-7">
         <div className="top">
-          <img src={office} alt="office" className="img-1" />
-          <div className="container-1" ref={this.container1Ref}>
-            <div className="header">Full‑sized keyboard</div>
-            <div className="context">
-              A full-sized keyboard designed for iPad Pro brings individual hard
-              key caps and a scissor mechanism with 1 millimetre of travel for a
-              responsive, comfortable and quiet typing experience. So go ahead;
-              write your novel, knock out a business plan or rip through your
-              inbox. And with backlit keys, you can get things done day or
-              night.
-            </div>
-          </div>
+          {/* White robot */}
+          <img src={robot} alt="office" className="robot" />
           <div className="container-2">
             <div className="header">Trackpad reimagined</div>
             <div className="context">
@@ -31,6 +23,20 @@ class MobileHomeSection7 extends Component {
               to go Home and switching between spaces.
             </div>
           </div>
+
+          {/* Dark robot */}
+          <img src={robot1} alt="office" className="robot-1" />
+          <div className="container-1">
+            <div className="header">Full‑sized keyboard</div>
+            <div className="context">
+              A full-sized keyboard designed for iPad Pro brings individual hard
+              key caps and a scissor mechanism with 1 millimetre of travel for a
+              responsive, comfortable and quiet typing experience. So go ahead;
+              write your novel, knock out a business plan or rip through your
+              inbox. And with backlit keys, you can get things done day or
+              night.
+            </div>
+          </div>
         </div>
         <div className="bottom">
           <div className="container-3">
@@ -39,7 +45,9 @@ class MobileHomeSection7 extends Component {
               The biggest thing to happen to the cursor since point and click.
             </div>
           </div>
-          <img src={backgroundImg} alt="abstract art" className="img-2" />
+          <video className="data-intelligence-mobile" autoPlay loop muted>
+            <source src={dataGif} type="video/webm" />
+          </video>
 
           <div className="container-4" ref={this.container4Ref}>
             <div className="header">Trackpad for iPadOS</div>
